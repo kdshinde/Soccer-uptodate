1f5438c78bcb2dd1fa71b3328197f96a1bdc3044
jsrikrish <jsrikrish@gmail.com>
2015-12-21 21:55:43 -0500

    Models for Teams and Players

---
+++ src/FootballMania/Models/PlayerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballMania.Models
+{
+    public class PlayerModel
+    {
+        public string name { get; set; }
+        public string position { get; set; }
+        public string jerseyNumber { get; set; }
+        public string dateOfBirth { get; set; }
+        public string nationality { get; set; }
+        public string contractUntil { get; set; }
+        public string marketValue { get; set; }
+    }
+}
--- src/FootballMania/Models/TeamModel.cs
+++ src/FootballMania/Models/TeamModel.cs
@@ -5,8 +5,59 @@ using System.Threading.Tasks;
 
 namespace FootballMania.Models
 {
-    public class Team
+
+    public class Teams
     {
-        // dummy checkin
+        public int count { get; set; }
+        public List<SingleTeam> teams { get; set; }
+        public Team_links _links { get; set; }
+    }
+
+    public class Team_links
+    {
+        public TeamSelf self { get; set; }
+        public TeamSoccerSeason players { get; set; }
+    } 
+
+    public class TeamSelf
+    {
+        public string href { get; set; }
+    }
+
+    public class TeamSoccerSeason
+    {
+        public string href { get; set; }
+    }
+
+    public class SingleTeam
+    {
+        public SingleTeam_links _links { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string shortName { get; set; }
+        public string squadMarketValue { get; set; }
+        public string crestUrl { get; set; }
+    }
+
+    public class SingleTeamSelf
+    {
+        public string href { get; set; }
+    }
+
+    public class SingleTeamPlayers
+    {
+        public string href { get; set; }
+    }
+
+    public class SingleTeamFixtures
+    {
+        public string href { get; set; }
+    }
+
+    public class SingleTeam_links
+    {
+        public SingleTeamSelf self { get; set; }
+        public SingleTeamPlayers players { get; set; }
+        public SingleTeamFixtures fixtures { get; set; }
     }
 }
